9ec148f29836c52344354f49f62ca8cf05bbac3b
asmdhabibullah <asmdhabibullah@yahoo.com>
2023-05-01 01:15:42 +0800

    ....

---
+++ db.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# setup DB
+if [ "$DBTYPE" = "postgres" ]
+then
+    psql -U postgres -w -c 'drop database tbench';
+    psql -U postgres -w -c 'create database tbench';
+elif [ "$DBTYPE" = "mysql" ]
+then
+    echo 'DROP DATABASE tbench' | mysql -u root -p$PASSWORD
+    echo 'CREATE DATABASE tbench' | mysql -u root -p$PASSWORD
+else
+    rm -f /tmp/db.sqlite3
+fi
